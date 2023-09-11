669b7ddddc6c70dd6ff1ce20996adebdfbac5742
Gustavo Iha <gustavoiha@gmail.com>
2023-09-11 10:41:35 -0300

    refactor: move routes to separate variable in router

--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,27 @@
 import { createRouter, createWebHistory } from 'vue-router'
+
 import DocumentationMain from '@/views/DocumentationMain.vue'
 import DocumentationDocumentationColors from '../views/Documentation/DocumentationColors.vue'
 import DocumentationButtons from '@/views/Documentation/DocumentationButtons.vue'
 
+const routes = [{
+  path: '/',
+  name: 'Documentation',
+  component: DocumentationMain,
+  children: [{
+    path: 'colors',
+    name: 'DocumentationColors',
+    component: DocumentationDocumentationColors
+  }, {
+    path: 'buttons',
+    name: 'DocumentationButtons',
+    component: DocumentationButtons
+  }]
+}]
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [{
-    path: '/',
-    name: 'Documentation',
-    component: DocumentationMain,
-    children: [{
-      path: 'colors',
-      name: 'DocumentationColors',
-      component: DocumentationDocumentationColors
-    }, {
-      path: 'buttons',
-      name: 'DocumentationButtons',
-      component: DocumentationButtons
-    }]
-  }]
+  routes
 })
 
 export default router
