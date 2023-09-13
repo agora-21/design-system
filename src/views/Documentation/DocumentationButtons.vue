d9ae323febb0eed0d83545f64b5ee56843389a46
Gustavo Iha <gustavoiha@gmail.com>
2023-09-13 13:39:04 -0300

    feat(ds-buttons): add descripiton to documentation

--- src/views/Documentation/DocumentationButtons.vue
+++ src/views/Documentation/DocumentationButtons.vue
@@ -4,59 +4,81 @@
       Buttons
     </h1>
 
-    <div class="documentation-buttons__group">
-      <ds-button color="plum" variant="filled">
-        Click here
-      </ds-button>
-      <ds-button color="plum" variant="outline">
-        Click here
-      </ds-button>
-    </div>
+    <section>
+      <h2 class="documentation-buttons__subject">
+        When to use
+      </h2>
 
-    <div class="documentation-buttons__group">
-      <ds-button color="indigo" variant="filled">
-        Click here
-      </ds-button>
-      <ds-button color="indigo" variant="outline">
-        Click here
-      </ds-button>
-    </div>
+      <p class="documentation-buttons__description">
+        A button communicates actions users can take to interact with the page.
+      </p>
+    </section>
 
-    <div class="documentation-buttons__group">
-      <ds-button color="blue" variant="filled">
-        Click here
-      </ds-button>
-      <ds-button color="blue" variant="outline">
-        Click here
-      </ds-button>
-    </div>
+    <section>
+      <h2 class="documentation-buttons__subject">
+        When not to use
+      </h2>
 
-    <div class="documentation-buttons__group">
-      <ds-button color="chocolate" variant="filled">
-        Click here
-      </ds-button>
-      <ds-button color="chocolate" variant="outline">
-        Click here
-      </ds-button>
-    </div>
+      <p class="documentation-buttons__description">
+        A button should never be used as a navigational element to change URLs.
+      </p>
+    </section>
 
-    <div class="documentation-buttons__group">
-      <ds-button color="green" variant="filled">
-        Click here
-      </ds-button>
-      <ds-button color="green" variant="outline">
-        Click here
-      </ds-button>
-    </div>
+    <section class="documentation-buttons__buttons-section">
+      <div class="documentation-buttons__group">
+        <ds-button color="plum" variant="filled">
+          Click here
+        </ds-button>
+        <ds-button color="plum" variant="outline">
+          Click here
+        </ds-button>
+      </div>
 
-    <div class="documentation-buttons__group">
-      <ds-button color="neutral" variant="filled">
-        Click here
-      </ds-button>
-      <ds-button color="neutral" variant="outline">
-        Click here
-      </ds-button>
-    </div>
+      <div class="documentation-buttons__group">
+        <ds-button color="indigo" variant="filled">
+          Click here
+        </ds-button>
+        <ds-button color="indigo" variant="outline">
+          Click here
+        </ds-button>
+      </div>
+
+      <div class="documentation-buttons__group">
+        <ds-button color="blue" variant="filled">
+          Click here
+        </ds-button>
+        <ds-button color="blue" variant="outline">
+          Click here
+        </ds-button>
+      </div>
+
+      <div class="documentation-buttons__group">
+        <ds-button color="chocolate" variant="filled">
+          Click here
+        </ds-button>
+        <ds-button color="chocolate" variant="outline">
+          Click here
+        </ds-button>
+      </div>
+
+      <div class="documentation-buttons__group">
+        <ds-button color="green" variant="filled">
+          Click here
+        </ds-button>
+        <ds-button color="green" variant="outline">
+          Click here
+        </ds-button>
+      </div>
+
+      <div class="documentation-buttons__group">
+        <ds-button color="neutral" variant="filled">
+          Click here
+        </ds-button>
+        <ds-button color="neutral" variant="outline">
+          Click here
+        </ds-button>
+      </div>
+    </section>
   </article>
 </template>
 
@@ -72,6 +94,21 @@ import DsButton from '@/components/DsButton.vue'
   &__title {
     color: var(--neutral-30);
     font: var(--font-title-1);
+  }
+
+  &__subject {
+    color: var(--neutral-40);
+    font: var(--font-caption);
+    margin-top: 20px;
+  }
+
+  &__description {
+    color: var(--neutral-30);
+    font: var(--font-body);
+  }
+
+  &__buttons-section {
+    margin-top: 40px;
   }
 
   &__group {
