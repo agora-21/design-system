42347c18d09c17271d0c54705d076c33e73ead4e
Gustavo Iha <gustavoiha@gmail.com>
2023-09-17 14:52:41 -0300

    feat: custom crollbar

--- src/views/DocumentationMain.vue
+++ src/views/DocumentationMain.vue
@@ -18,11 +18,24 @@ import DsSideBar from '@/components/DsSideBar.vue'
   grid-template-columns: 320px 1fr;
   grid-template-rows: 100%;
   height: 100%;
-  overflow-y: auto;
   width: 100%;
 
   &__content {
     display: grid;
+    overflow-y: auto;
+
+    &::-webkit-scrollbar {
+      width: 4px;
+    }
+
+    &::-webkit-scrollbar-track {
+      background: linear-gradient(0deg, var(--blue-100) 0%, var(--chocolate-100) 50%, var(--green-100) 100%);
+    }
+
+    &::-webkit-scrollbar-thumb {
+      background-color: var(--neutral-110);
+      border-radius: 2px;
+    }
   }
 }
 </style>
